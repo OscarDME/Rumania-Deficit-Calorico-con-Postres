74000355508b65878aca61694b27b553f487557a
oscardme <oscarmadriz25@gmail.com>
2026-04-27 23:01:56 -0600

    Localize hardcoded Spanish to native language

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className={poppins.variable}>
+    <html lang="ro" className={poppins.variable}>
       <head>
         <link
           rel="icon"
